f54f29b78db065d4cc0c07b81cca876dd5e9c6ad
Tatia burdett <tatiaburdett@gmail.com>
2021-05-02 13:01:54 -0700

    write all endpoints

--- src/address/address-router.js
+++ src/address/address-router.js
@@ -28,5 +28,82 @@ addressRouter
       })
       .catch(next)
   })
+  .post(jsonParser, (res, res, next) => {
+    const { from_date, to_date, street_address, unit, city, abb_state, zipcode, current, userId} = req.body
+    const newAddress = { from_date, to_date, street_address, unit , city, abb_state, zipcode, current, userId }
+    const requiredFields = { from_date, street_address, city, abb_state, zipcode, userId }
+
+    for (const [key, value] of Object.entries(requiredFields)) {
+      if (!value) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in response body` }
+        })
+      }
+    }
+
+    AddressService.inserAddress(
+      req.app.get('db'),
+      newAddress
+    )
+      .then(address => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${address.id}`))
+          .json(serializeAddress(address))
+      })
+      .catch(next)
+  })
+
+addressRouter
+  .route('/:addressId')
+  .all((req, res, next) => {
+    AddressService.getById(
+      req.app.get('db'),
+      req.params.id
+    )
+      .then(address => {
+        if (!address) {
+          return res.status(404).json({
+            error: { message: `Address doesn't exist` }
+          })
+        }
+        res.address = address
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeAddress(res.address))
+  })
+  .delete((req, res, next) => {
+    AddressService.deleteAddress(
+      req.app.get('db'),
+      req.params.id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { from_date, to_date, street_address, unit, city, abb_state, zipcode, current } = req.body
+    const addressToUpdate = { from_date, to_date, street_address, unit, city, abb_state, zipcode, current }
+
+    const numberOfValues = Object.values(addressToUpdate).filter(Boolean).length
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: { message: `Request body must contain a 'from_date', 'to_date", 'street_address', 'unit', 'city', 'abb_state', 'zipcode', or 'current'` }
+      })
+    }
+    AddressService.updateAddress(
+      req.app.get('db'),
+      req.params.id,
+      addressToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
 
 module.exports = addressRouter
