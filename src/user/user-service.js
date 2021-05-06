64b1c4d19847ae0ccbf897c224c3bcff4aed04f8
Tatia burdett <tatiaburdett@gmail.com>
2021-05-05 19:57:11 -0700

    add user-service methods

--- src/user/user-service.js
+++ src/user/user-service.js
@@ -1,6 +1,12 @@
+const bcrypt = require('bcryptjs')
+const xss = require('xss')
+
 const UserService = {
-  getAllUsers(knex) {
-    return knex.select('*').from('lodgelog_users')
+  hasUserWithUsername(knex, username) {
+    return knex('lodgelog_users')
+      .where({ username })
+      .first()
+      .then(user => !!user)
   },
 
   insertUser(knex, newUser) {
@@ -8,56 +14,31 @@ const UserService = {
       .insert(newUser)
       .into('lodgelog_users')
       .returning('*')
-      .then(rows => {
-        return rows[0]
-      })
+      .then(([user]) => user)
   },
 
-  getById(knex, id) {
-    return knex
-      .from('lodgelog_users')
-      .select('*')
-      .where('id', id)
-      .first()
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password must be longer than 8 characters'
+    }
+    if (password.length > 72) {
+      return 'Password must be less than 72 characters'
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces'
+    }
+    return null
   },
 
-  getAddressForUsers(knex, userid) {
-    return knex
-      .from('lodgelog_address AS add')
-      .select('*')
-      .where('add.userid', userid)
-      .leftJoin(
-        'lodgelog_users AS usr',
-        'add.userid',
-        'usr.id'
-      )
-      .groupBy('add.id', 'usr.id')
+  hashPassword(password) {
+    return bcrypt.hash(password, 12)
   },
 
-  deleteUser(knex, id) {
-    return knex('lodgelog_users')
-      .where({ id })
-      .delete()
-  },
-
-  updateUser(knex, id, newUserFields) {
-    return knex('lodgelog_users')
-      .where({ id })
-      .update(newUserFields)
-  },
-
-  serializeUserAddress(address) {
+  serializeUser(user) {
     return {
-      id: address.id,
-      from_date: address.from_date,
-      to_date: address.to_date,
-      street_address: address.street_address,
-      unit: address.unit,
-      city: address.city,
-      abb_state: address.abb_state,
-      zipcode: address.zipcode,
-      current: address.current,
-      userId: address.userid
+      id: user.id,
+      username: xss(user.username),
+      date_created: new Date(user.date_created)
     }
   }
 }
