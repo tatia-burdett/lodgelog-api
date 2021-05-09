36086d81692ef5cc6aa2ffd134f02babb4927949
Tatia burdett <tatiaburdett@gmail.com>
2021-05-09 10:21:36 -0700

    add new endpoint to tests

--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -1,8 +1,9 @@
 const knex = require('knex')
+const supertest = require('supertest')
 const app = require('../src/app')
 const fixtures = require('./test_fixtures')
 
-describe('Protected endpoints', () => {
+describe.only('Protected endpoints', () => {
   let db
 
   before('Make knex instance', () => {
@@ -19,9 +20,43 @@ describe('Protected endpoints', () => {
 
   afterEach('cleanup', () => db.raw('TRUNCATE lodgelog_address, lodgelog_users RESTART IDENTITY CASCADE'))
 
+  const testUsers = fixtures.makeUsersArray()
+
   const protectedEndpoints = [
     {
-      name: 'GET'
+      name: 'GET /api/address/user/:userid',
+      path: '/api/address/user/1',
+      method: supertest(app).get
+    },
+    {
+      name: 'POST /api/auth/refresh',
+      path: '/api/auth/refresh',
+      method: supertest(app).post
     }
   ]
+
+  protectedEndpoints.forEach(endpoint => {
+    describe(endpoint.name, () => {
+
+      it(`responds 401 'Missing bearer token' when no bearer token`, () => {
+        return endpoint.method(endpoint.path)
+          .expect(401, { error: 'Missing bearer token' })
+      })
+
+      it(`responds with 401 'Unauthorized request' when invalid JWT secret`, () => {
+        const validUser = testUsers[0]
+        const invalidSecret = 'bad-secret'
+        return endpoint.method(endpoint.path)
+          .set('Authorization', fixtures.makeAuthHeader(validUser, invalidSecret))
+          .expect(401, { error: 'Unauthorized request' })
+      })
+
+      it(`responds with 401 'Unauthorized request' when invalid sub in payload`, () => {
+        const invalidUser = { username: 'invaliduser', id: 1 }
+        return endpoint.method(endpoint.path)
+          .set('Authorization', fixtures.makeAuthHeader(invalidUser))
+          .expect(401, { error: 'Unauthorized request' })
+      })
+    })
+  })
 })
